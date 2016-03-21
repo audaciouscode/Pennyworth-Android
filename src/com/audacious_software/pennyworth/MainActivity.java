2f218c78880a7ea8597f0a6c0c49fd42996fe622
Chris J. Karr <chris@audacious-software.com>
2016-03-21 09:18:22 -0500

    Initial integration with PassiveDataKit library

---
+++ src/com/audacious_software/pennyworth/MainActivity.java
@@ -0,0 +1,13 @@
+package com.audacious_software.pennyworth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.setContentView(R.layout.activity_main);
+    }
+}
