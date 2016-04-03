a2e982da028f18fdccc4b2d7dfb1e9de5068e278
Chris J. Karr <chris@audacious-software.com>
2016-04-03 17:07:54 -0500

    Passive Data Kit integration and testing

--- src/com/audacious_software/pennyworth/MainActivity.java
+++ src/com/audacious_software/pennyworth/MainActivity.java
@@ -1,7 +1,12 @@
 package com.audacious_software.pennyworth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.audacious_software.passive_data_kit.PassiveDataKit;
+import com.audacious_software.passive_data_kit.activities.DiagnosticsActivity;
 import com.audacious_software.passive_data_kit.activities.PdkActivity;
 
 public class MainActivity extends PdkActivity
@@ -10,5 +15,26 @@ public class MainActivity extends PdkActivity
         super.onCreate(savedInstanceState);
 
         this.setContentView(R.layout.activity_main);
+
+        PassiveDataKit.getInstance(this).start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        this.getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_diagnostics) {
+            Intent diagnosticsIntent = new Intent(this, DiagnosticsActivity.class);
+            this.startActivity(diagnosticsIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
