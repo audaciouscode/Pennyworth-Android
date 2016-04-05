b3f4bd8743f4ca8cb81b8306ffc417ad08bb0002
Chris J. Karr <chris@audacious-software.com>
2016-04-05 10:18:25 -0500

    Updates to support framework development

--- src/com/audacious_software/pennyworth/MainActivity.java
+++ src/com/audacious_software/pennyworth/MainActivity.java
@@ -2,12 +2,15 @@ package com.audacious_software.pennyworth;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.audacious_software.passive_data_kit.PassiveDataKit;
 import com.audacious_software.passive_data_kit.activities.DiagnosticsActivity;
+import com.audacious_software.passive_data_kit.activities.DataStreamActivity;
 import com.audacious_software.passive_data_kit.activities.PdkActivity;
+import com.audacious_software.passive_data_kit.generators.Generators;
 
 public class MainActivity extends PdkActivity
 {
@@ -17,6 +20,13 @@ public class MainActivity extends PdkActivity
         this.setContentView(R.layout.activity_main);
 
         PassiveDataKit.getInstance(this).start();
+
+        Generators.getInstance(this).addNewDataPointListener(new Generators.NewDataPointListener() {
+            @Override
+            public void onNewDataPoint(String identifier, Bundle data) {
+                Log.e("Pennyworth", "DATA[" + identifier + "] = " + data.toString());
+            }
+        });
     }
 
     @Override
@@ -33,6 +43,10 @@ public class MainActivity extends PdkActivity
         if (id == R.id.action_diagnostics) {
             Intent diagnosticsIntent = new Intent(this, DiagnosticsActivity.class);
             this.startActivity(diagnosticsIntent);
+        }
+        else if (id == R.id.action_data_stream) {
+            Intent dataIntent = new Intent(this, DataStreamActivity.class);
+            this.startActivity(dataIntent);
         }
 
         return super.onOptionsItemSelected(item);
